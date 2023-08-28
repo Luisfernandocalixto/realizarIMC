10e812496955ba1e0eca4b4c3abb123a7ae764b4
Fernan <luisfernandopenacalixtro@gmail.com>
2023-08-28 12:56:06 -0600

    Index, estilo css y script

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let mensaje = document.getElementById('Resultado') // mensaje con el resultado => id="Resultado"
+let mensajeEstado = document.getElementById('estado') // estado de acuerdo al indicador => id="estado"
+let modal = document.getElementById('ventanaModal') // Referencia para nuestra ventana modal => id="ventanaModal"
+
+document.getElementById('formulario').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    let pesoIngresado = document.getElementById('Peso').value // Entrada de peso kg
+    let alturaIngresada = document.getElementById('Altura').value // Entrada de altura metros
+    let calcular = eval(pesoIngresado / (alturaIngresada * alturaIngresada))
+    mensaje.textContent = calcular.toFixed(1) //digitos para los decimales aceptados
+    mensaje.style.color = 'orangered'
+    if (calcular === 0 || isNaN(calcular)) {
+        mensaje.textContent = 0
+        mensajeEstado.textContent = ''
+        modal.style.display = 'block'
+    } else if (calcular <= 18.4) {
+        mensajeEstado.textContent = 'Peso bajo'
+        mensajeEstado.style.color = 'Blue'
+    } else if (calcular >= 18.5 && calcular <= 25.0) {
+        mensajeEstado.textContent = 'Normal'
+        mensajeEstado.style.color = 'Green'
+    } else if (calcular >= 25.1) {
+        mensajeEstado.textContent = 'Sobrepeso'
+        mensajeEstado.style.color = 'Red'
+    }
+
+    if (!pesoIngresado.textContent && !alturaIngresada.textContent) {
+        document.getElementById('formulario').reset(); // Limpiar los campos del form
+    }
+
+});
+
+/* Modal => Permitir mostrar la recomendación y desaparecer cuando se toca cualquier parte de la pantalla */
+window.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+
+
